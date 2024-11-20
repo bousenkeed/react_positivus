e90da6174f33dad1bca0b672f6f63273feaf8d47
ant.sakh38 <ant.sakh38@gmail.com>
2024-11-20 14:54:41 +0300

    styles ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <h1>Hello World!</h1>
+            <p>
+                aksdf alskdfjlasdkjf asdlkfjaslkdjfaejwij a;lskdf a;lsdkfj
+                ;laskdjfWA:LKSDJ asdlk j:LKASjdl;akjsd
+            </p>
+            <p>
+                aksdf alskdfjlasdkjf asdlkfjaslkdjfaejwij a;lskdf a;lsdkfj
+                ;laskdjfWA:LKSDJ asdlk j:LKASjdl;akjsd
+            </p>
+            <p>
+                aksdf alskdfjlasdkjf asdlkfjaslkdjfaejwij a;lskdf a;lsdkfj
+                ;laskdjfWA:LKSDJ asdlk j:LKASjdl;akjsd
+            </p>
+        </div>
+    );
 }
 
 export default App;
